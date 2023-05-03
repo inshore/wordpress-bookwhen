afddba70ab1e8fbf7031e8deac23dd97902bddbf
Daniel Mullin <email@danielmullin.com>
2023-05-03 14:39:18 +0200

    plugin installs

---
+++ inshore-bookwhen/inshore-bookwhen.php
@@ -0,0 +1,28 @@
+<?php 
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/inshore/wordpress-bookwhen/
+ * @since             1.0.0
+ * @package           inshore-bookwhen 
+ *
+ * @wordpress-plugin
+ * Plugin Name:       inShore Wordpress Bookwhen
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       WordPress plugin built on the inshore/bookwhen PHP API SDK package
+ * Version:           1.0.0
+ * Author:            inShore Ltd
+ * Author URI:        https://einshore.je/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       pinshore-bookwhen
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
